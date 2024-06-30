0937fb27687b24fa450df5c524ff97bff7691b9c
sbbaldwin <bal20020@byui.edu>
2024-06-29 20:10:16 -0600

    logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,13 @@ export default function Page() {
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-center justify-center rounded-lg bg-customGreen p-4 md:h-52">
         {/* Insert logo here */}
+        <Image
+          src="/logo.png"
+          alt="Handcrafted Haven Logo"
+          width={150}
+          height={150}
+          className="object-contain"
+        />
         <h1 className="text-2xl md:text-5xl font-bold text-center text-white mt-4 mb-2">
           Handcrafted Haven
         </h1>
