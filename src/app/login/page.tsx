1a2ddf30bba174d00d8c6072a0a902484034bf75
ramakrishnadas <rama.k.d.b.p@gmail.com>
2024-07-09 14:05:03 -0600

    changed apostrophe to &apos;

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -78,7 +78,7 @@ export default function LoginPage() {
                     </button>
                 </form>
                 <div className="mt-4 text-center text-sm text-gray-600">
-                    Don't have an account?{" "}
+                    Don&apos;t have an account?{" "}
                     <Link href="/register" className="font-medium text-indigo-600 hover:text-indigo-500">
                         Sign up
                     </Link>
