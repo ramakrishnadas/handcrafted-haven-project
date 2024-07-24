32ef047a27a998e67f32ec1b7a672cb7b5d77dd7
sbbaldwin <bal20020@byui.edu>
2024-07-23 18:59:13 -0600

    reviews section styles

--- src/app/ui/product/reviewsAndRatings.tsx
+++ src/app/ui/product/reviewsAndRatings.tsx
@@ -1,16 +1,36 @@
 import Image from "next/image";
 import { getReviewsAndRatingsByProduct } from "@/app/lib/data";
 
-export default async function ReviewsAndRatings({productId}: { productId: string;}) {
+export default async function ReviewsAndRatings({ productId }: { productId: string; }) {
 
-	const reviewsAndRatings = await getReviewsAndRatingsByProduct(productId);
+  const reviewsAndRatings = await getReviewsAndRatingsByProduct(productId);
+
+  const averageRating =
+    reviewsAndRatings?.reduce((sum, review) => sum + review.rating, 0) / reviewsAndRatings?.length || 0;
+
 
   return (
     <div className="container mx-auto p-8 flex flex-col items-center justify-center">
       <ul className="bg-white rounded-lg shadow-md p-8">
-        <h1 className="text-center text-3xl font-bold mb-8 text-gray-900">
+        <h1 className="text-center text-3xl font-bold mb-4 text-gray-900">
           Reviews and Ratings
         </h1>
+        <div className="text-center mb-2 text-xl font-semibold text-gray-700">
+          {averageRating.toFixed(1)}
+        </div>
+        <div className="flex justify-center mb-8">
+          {Array.from({ length: 5 }).map((_, index) => (
+            <svg
+              key={index}
+              className="w-6 h-6 text-yellow-500"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.1 3.386a1 1 0 00.95.69h3.566c.969 0 1.371 1.24.588 1.81l-2.89 2.1a1 1 0 00-.364 1.118l1.1 3.386c.3.921-.755 1.688-1.54 1.118l-2.89-2.1a1 1 0 00-1.175 0l-2.89 2.1c-.785.57-1.84-.197-1.54-1.118l1.1-3.386a1 1 0 00-.364-1.118l-2.89-2.1c-.784-.57-.38-1.81.588-1.81h3.566a1 1 0 00.95-.69l1.1-3.386z" />
+            </svg>
+          ))}
+        </div>
         {reviewsAndRatings?.map((reviewAndRating) => (
           <li
             key={reviewAndRating.product_id}
@@ -28,7 +48,7 @@ export default async function ReviewsAndRatings({productId}: { productId: string
             <div className="flex flex-col sm:flex-row justify-between items-center w-full">
               <div className="mb-4 sm:mb-0 sm:mr-6">
                 <p className="text-lg font-semibold text-gray-700">
-									{reviewAndRating.user_name}
+                  {reviewAndRating.user_name}
                 </p>
                 <p className="text-lg font-semibold text-gray-700">
                   {reviewAndRating.review}
